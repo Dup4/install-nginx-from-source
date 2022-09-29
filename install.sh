57303b897ca714073d419816de44fee9049e25f7
Dup4 <lyuzhi.pan@gmail.com>
2022-09-29 12:47:13 +0800

    chore: add more module

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 
 ./configure \
     --prefix=/usr/local/nginx \
+    --with-http_realip_module \
+    --with-http_v2_module \
     --with-http_ssl_module \
+    --with-http_sub_module \
     --with-pcre=../pcre2-10.40 \
     --with-zlib=../zlib-1.2.12 \
     --with-openssl=../openssl-openssl-3.0.5
