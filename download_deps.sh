45022dd37b5c3dbf390a924e4d9968312c6f633f
Dup4 <lyuzhi.pan@gmail.com>
2022-09-29 10:07:49 +0800

    feat: download deps script

---
+++ download_deps.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+wget https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.40/pcre2-10.40.zip
+
+wget http://zlib.net/zlib-1.2.12.tar.gz
+
+wget https://github.com/openssl/openssl/archive/refs/tags/openssl-3.0.5.zip
